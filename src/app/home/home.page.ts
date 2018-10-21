4dcf117bb07ade30ddf2832d1fb03da6da5af12a
mduraes1994 <mduraes1994@gmail.com>
2018-10-21 00:55:36 -0300

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  @ViewChild('map') mapRef: ElementRef;
+  private map: google.maps.Map;
+  private mapOptions: google.maps.MapOptions;
+
+  constructor() {}
+
+  private initMap(): void {    
+    const latLng: google.maps.LatLng = new google.maps.LatLng(-19.919157, -43.938547);
+    this.mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeControl: false,
+      fullscreenControl: false,
+      streetViewControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      gestureHandling: 'cooperative'
+    };
+    this.map = new google.maps.Map(this.mapRef.nativeElement, this.mapOptions);
+
+    new google.maps.Marker({
+      position: new google.maps.LatLng(-19.919157, -43.938547),
+      map: this.map,
+    });
+
+  };
+
+  ngOnInit() {
+    this.initMap();
+  }
 
 }
